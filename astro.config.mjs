180b767773a43e15f098c04b50053d5ae6419ed1
Zehao Lu <luzehao1998@gmail.com>
2025-09-01 18:37:05 -0700

    add base in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://zehaolu98.github.io",
-	base: "/",
+	base: "/Blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
