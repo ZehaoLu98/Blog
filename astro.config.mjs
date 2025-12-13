d17d30fbba02f4dbe890f663fc03bec8388357fb
Zehao Lu <luzehao1998@gmail.com>
2025-12-13 01:43:12 -0800

    Update site URL in configuration and CNAME file to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "http://www.labmem003.blog/",
+	site: "http://www.zehaoblog.com/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
